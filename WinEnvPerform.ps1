94206ec9eef375e18e0b90797358cd9e6764008f
Kiara Arosemena <lynmaru@gmail.com>
2024-05-21 15:52:27 -0500

    Se a cambiado varias lineas donde si funcionan las modificaciones, la unica que no funciona es la de quitar los efectos de transparecia que no se logra desactivar

--- WinEnvPerform.ps1
+++ WinEnvPerform.ps1
@@ -1,29 +1,40 @@
-# Cambiar a modo oscuro
-New-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "AppsUseLightTheme" -Value 0 -PropertyType DWORD -Force
-New-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "SystemUsesLightTheme" -Value 0 -PropertyType DWORD -Force
-
-# Desactivar efectos de transparencia
-New-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "EnableTransparency" -Value 0 -PropertyType DWORD -Force
-
-# Desactivar efectos visuales
-Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects" -Name VisualFXSetting -Value 2
-
-# Activar siempre las barras de desplazamiento
-Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "UserPreferencesMask" -Value 0
-
-# Desactivar efectos de animación
-Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "UserPreferencesMask" -Value 0x10
-
-# Quitar widgets de la barra de tareas menos Copilot
-$widgetsToRemove = @("Weather","News","Microsoft.ToDo","Photos","Bing","Microsoft Store","Spotify","Microsoft.Office.OneNote_8wekyb3d8bbwe!microsoft.unity.NotebookPlugin","Microsoft.Microsoft3DViewer_8wekyb3d8bbwe!App")
-$regPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People"
-
-foreach ($widget in $widgetsToRemove) {
-    New-ItemProperty -Path $regPath -Name $widget -Value 0 -PropertyType DWORD -Force
+# Forzar la ejecución de PowerShell como Administrador
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    $arguments = "& '" + $myinvocation.mycommand.definition + "'"
+    Start-Process powershell -Verb runAs -ArgumentList $arguments
+    Break
 }
 
+# Cambiar a modo oscuro
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "AppsUseLightTheme" -Value 0 -Type DWORD -Force
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "SystemUsesLightTheme" -Value 0 -Type DWORD -Force
+
+# Desactivar efectos de transparencia
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "EnableTransparency" -Value 0 -Type DWORD -Force
+
+# Desactivar efectos visuales
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects" -Name "VisualFXSetting" -Value 2 -Type DWORD -Force
+
+# Activar siempre las barras de desplazamiento
+Set-ItemProperty -Path "HKCU:\Control Panel\Accessibility" -Name "DynamicScrollbars" -Value 0 -Type DWORD -Force
+
+# Desactivar efectos de animación
+Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "WindowMetrics" -Value 0 -Type DWORD -Force
+Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "MinAnimate" -Value 0 -Type DWORD -Force
+
+# Desactivar elementos específicos de la barra de tareas
+# Desactivar Vista de Tareas
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ShowTaskViewButton" -Value 0 -Type DWORD -Force
+
+# Desactivar Widgets
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarDa" -Value 0 -Type DWORD -Force
+
+# Desactivar Chat
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarMn" -Value 0 -Type DWORD -Force
+
 # Cambiar el comportamiento de la barra de tareas a alineación hacia la izquierda
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarAl" -Value 0
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarAl" -Value 0 -Type DWORD -Force
 
 # Reiniciar el Explorador de Windows para aplicar los cambios
 Stop-Process -Name explorer -Force
+Start-Process explorer
