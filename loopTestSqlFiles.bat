fe047e636b6ee9fb07e0ed9dec93654e45076443
k1ara <lynmaru@gmail.com>
2024-12-05 08:41:14 -0500

    script para testear integridad de los backups de sql
    script para testear integridad de los backups de sql

---
+++ loopTestSqlFiles.bat
@@ -0,0 +1,7 @@
+set "server=localhost"
+
+:: Recorrer todos los archivos .bak en el directorio de scripts
+FOR %%I IN (*.BAK) DO (
+    sqlcmd -E -S %server% -Q "RESTORE VERIFYONLY FROM DISK = 'C:\Test\%%I' WITH CHECKSUM"
+)
+pause
